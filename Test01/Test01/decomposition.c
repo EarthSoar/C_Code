0bfb48ba01d0b6214d5e235aca494ccc7ef81251
EarthSoar <1562660267@qq.com>
2018-10-01 18:20:03 +0800

    books

---
+++ Test01/Test01/decomposition.c
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+//分解一个数为几个数相乘。  16 = 2*2*2*2;  17=17;   14 = 2*7  6= 2*3;
+//int main(){
+//	int n, i;
+//	scanf("%d", &n);
+//	printf("%d=", n);
+//	for (i = 2; i <= n; i++){
+//		while (i != n){
+//			if (n%i == 0){
+//				printf("%d*", i);
+//				n = n / i;
+//			}
+//			else
+//				break;
+//		}
+//	}
+//	printf("%d", n);
+//	system("pause");
+//	return 0;
+//}
---
+++ Test01/Test01/test2.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	printf("\41\n");
+	system("pause");
+	return 0;
+}
